16384ee2a00ecf28885cd649513c87b4c95b9907
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 20:59:10 -0300

    projeto refeito

---
+++ src/main/java/br/com/fiap/roardemo/RoarDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.roardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoarDemoApplication {
+    // TOKEN DO GITHUB AUTH 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
+    public static void main(String[] args) {
+        SpringApplication.run(RoarDemoApplication.class, args);
+    }
+
+}
